571575329f09769902b3f7d63386fa5cdcd040ed
satsangamusa <venkataavula@venkata.local>
2020-07-18 17:41:29 -0400

    easy navigation

--- src/app/content/content.page.ts
+++ src/app/content/content.page.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { GlobalService } from 'src/app/global.service';
-import { PopoverController } from '@ionic/angular';
+import { PopoverController, IonContent } from '@ionic/angular';
 import { SettingsModalPage } from 'src/app/settings-modal/settings-modal.page';
-
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 @Component({
   selector: 'app-content',
   templateUrl: './content.page.html',
   styleUrls: ['./content.page.scss'],
 })
 export class ContentPage implements OnInit {
+  @ViewChild(IonContent) content: IonContent;
   constructor(public global: GlobalService,public pop:PopoverController) {
 
   }
 
   ngOnInit() {
   }
-  ionViewDidEnter() {
-  }
+  
+  goToTop() {
+    this.content.scrollToTop(0);
+    this.global.currentPage=this.global.currentPage+1;
+    }
   async openSettings(){
     
   const popover = await this.pop.create({
