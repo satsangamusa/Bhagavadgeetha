e7779509badbf4c3363af1681ae8cdc5ead5013d
satsangamusa <33586258+satsangamusa@users.noreply.github.com>
2024-07-15 22:01:51 -0400

    text corrections and zoom feature

--- android/app/src/main/java/org/thraithashakam/bhagavadgeetha/telugu/MainActivity.java
+++ android/app/src/main/java/org/thraithashakam/bhagavadgeetha/telugu/MainActivity.java
@@ -1,5 +1,14 @@
 package org.thraithashakam.bhagavadgeetha.telugu;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        registerPlugin(ZoomPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'org.thraithashakam.bhagavadgeetha.telugu',
   appName: 'భగవద్గీత',
-  webDir: 'www/browser'
+  webDir: 'www/browser',
+  zoomEnabled:true
 };
 
 export default config;
